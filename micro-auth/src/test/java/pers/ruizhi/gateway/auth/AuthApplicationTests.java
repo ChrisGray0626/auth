a2902dad65ac6ccceaf81149e93f2712e8d8f36c
RuizhiLi <ruizhi_li00@163.com>
2024-08-13 10:15:08 +0800

    fix(auth): AuthApplicationTests fix(auth): dialect of jpa build(common): spring-boot-maven-plugin fix(course): dialect of jpa

--- micro-auth/src/test/java/pers/ruizhi/gateway/auth/AuthApplicationTests.java
+++ micro-auth/src/test/java/pers/ruizhi/gateway/auth/AuthApplicationTests.java
@@ -1,10 +1,8 @@
 package pers.ruizhi.gateway.auth;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootTest
 class AuthApplicationTests {
 
     @Test
